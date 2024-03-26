1b2f0ac3c7c97a80298052a40320e8357ac6869b
Daniel <danielperezalonso30@gmail.com>
2024-03-26 18:27:22 +0100

    commit terminado, falta control de errores
    Signed-off-by: Daniel <danielperezalonso30@gmail.com>

--- tema8_ej05/src/tema8_ej05/Tienda.java
+++ tema8_ej05/src/tema8_ej05/Tienda.java
@@ -2,6 +2,7 @@ package tema8_ej05;
 
 import java.awt.EventQueue;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,6 +11,7 @@ import javax.swing.JFrame;
 import javax.swing.JTable;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
@@ -28,9 +30,10 @@ public class Tienda {
 	private JTextField txtNombre;
 	private JTextField txtUnidades;
 	private JTextField txtNuevoprecio;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField txtStock;
+	private JTextField textFieldUdVender;
 	Connection con;
+	int profit = 0;
 
 	/**
 	 * Launch the application.
@@ -64,14 +67,6 @@ public class Tienda {
 		frmTienda.setBounds(100, 100, 854, 670);
 		frmTienda.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmTienda.getContentPane().setLayout(null);
-
-		JButton btnMostrar = new JButton("Mostrar");
-		btnMostrar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnMostrar.setBounds(162, 39, 117, 25);
-		frmTienda.getContentPane().add(btnMostrar);
 
 		JLabel lblNuevoProducto = new JLabel("Nuevo producto:");
 		lblNuevoProducto.setBounds(12, 241, 125, 15);
@@ -113,14 +108,6 @@ public class Tienda {
 		lblUnidades.setBounds(236, 295, 87, 15);
 		frmTienda.getContentPane().add(lblUnidades);
 
-		JButton btnAnyadir = new JButton("Añadir");
-		btnAnyadir.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnAnyadir.setBounds(175, 324, 117, 25);
-		frmTienda.getContentPane().add(btnAnyadir);
-
 		JLabel lblBorrarProducto = new JLabel("Borrar producto:");
 		lblBorrarProducto.setBounds(12, 366, 125, 15);
 		frmTienda.getContentPane().add(lblBorrarProducto);
@@ -134,12 +121,6 @@ public class Tienda {
 		frmTienda.getContentPane().add(comboBoxBorrar);
 
 		JButton btnEliminar = new JButton("Eliminar");
-		btnEliminar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnEliminar.setBounds(217, 388, 117, 25);
-		frmTienda.getContentPane().add(btnEliminar);
 
 		JLabel lblActualizarPrecio = new JLabel("Actualizar precio:");
 		lblActualizarPrecio.setBounds(485, 76, 136, 15);
@@ -162,14 +143,6 @@ public class Tienda {
 		frmTienda.getContentPane().add(txtNuevoprecio);
 		txtNuevoprecio.setColumns(10);
 
-		JButton btnActualizarPrecio = new JButton("Actualizar precio");
-		btnActualizarPrecio.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnActualizarPrecio.setBounds(690, 98, 164, 25);
-		frmTienda.getContentPane().add(btnActualizarPrecio);
-
 		JLabel lblIncrementarStock = new JLabel("Incrementar stock:");
 		lblIncrementarStock.setBounds(485, 241, 136, 15);
 		frmTienda.getContentPane().add(lblIncrementarStock);
@@ -178,51 +151,35 @@ public class Tienda {
 		lblEligeProductoStock.setBounds(495, 267, 117, 15);
 		frmTienda.getContentPane().add(lblEligeProductoStock);
 
-		JComboBox comboBoxActualizar_1 = new JComboBox();
-		comboBoxActualizar_1.setBounds(604, 262, 58, 24);
-		frmTienda.getContentPane().add(comboBoxActualizar_1);
+		JComboBox comboBoxStock = new JComboBox();
+		comboBoxStock.setBounds(604, 262, 58, 24);
+		frmTienda.getContentPane().add(comboBoxStock);
 
 		JLabel lblUnidadesAdquirids = new JLabel("Unidades adquiridas: ");
 		lblUnidadesAdquirids.setBounds(495, 306, 193, 15);
 		frmTienda.getContentPane().add(lblUnidadesAdquirids);
 
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(646, 304, 79, 19);
-		frmTienda.getContentPane().add(textField);
-
-		JButton btnActualizarStock = new JButton("Actualizar stock");
-		btnActualizarStock.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnActualizarStock.setBounds(686, 263, 168, 25);
-		frmTienda.getContentPane().add(btnActualizarStock);
+		txtStock = new JTextField();
+		txtStock.setColumns(10);
+		txtStock.setBounds(646, 304, 79, 19);
+		frmTienda.getContentPane().add(txtStock);
 
 		JLabel lblProductoAVender = new JLabel("Producto a vender:");
 		lblProductoAVender.setBounds(495, 371, 144, 15);
 		frmTienda.getContentPane().add(lblProductoAVender);
 
-		JComboBox comboBoxActualizar_2 = new JComboBox();
-		comboBoxActualizar_2.setBounds(631, 366, 58, 24);
-		frmTienda.getContentPane().add(comboBoxActualizar_2);
+		JComboBox comboBoxVender = new JComboBox();
+		comboBoxVender.setBounds(631, 366, 58, 24);
+		frmTienda.getContentPane().add(comboBoxVender);
 
 		JLabel lblUnidadesAVender = new JLabel("Unidades a vender:");
 		lblUnidadesAVender.setBounds(495, 410, 144, 15);
 		frmTienda.getContentPane().add(lblUnidadesAVender);
 
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(631, 408, 87, 19);
-		frmTienda.getContentPane().add(textField_1);
-
-		JButton btnVender = new JButton("Vender");
-		btnVender.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnVender.setBounds(733, 388, 108, 25);
-		frmTienda.getContentPane().add(btnVender);
+		textFieldUdVender = new JTextField();
+		textFieldUdVender.setColumns(10);
+		textFieldUdVender.setBounds(631, 408, 87, 19);
+		frmTienda.getContentPane().add(textFieldUdVender);
 
 		JLabel lblVenta = new JLabel("Venta:");
 		lblVenta.setBounds(485, 344, 70, 15);
@@ -243,10 +200,14 @@ public class Tienda {
 		labelEuro.setBounds(764, 437, 70, 15);
 		frmTienda.getContentPane().add(labelEuro);
 
+		DefaultTableModel model = new DefaultTableModel();
+		JTable table = new JTable(model);
+		table.setEnabled(false);
+
+		JScrollPane scrollPane;
 		try {
 			con = ConnectionSingleton.getConnection();
 
-			DefaultTableModel model = new DefaultTableModel();
 			model.addColumn("Código");
 			model.addColumn("Nombre");
 			model.addColumn("Precio");
@@ -263,17 +224,280 @@ public class Tienda {
 
 				model.addRow(row);
 			}
-			JTable table = new JTable(model);
+			stmt.close();
+			rs.close();
 			table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
 			table.setBounds(0, 0, 100, 100);
-			JScrollPane scrollPane = new JScrollPane(table);
+			scrollPane = new JScrollPane(table);
 			scrollPane.setBounds(22, 76, 445, 153);
 
 			frmTienda.getContentPane().add(scrollPane);
+
+			Statement stmt2 = con.createStatement();
+			ResultSet rs2 = stmt2.executeQuery("SELECT codigo FROM productos");
+
+			while (rs2.next()) {
+				int cod = rs2.getInt("codigo");
+				comboBoxActualizar.addItem(cod);
+				comboBoxBorrar.addItem(cod);
+				comboBoxStock.addItem(cod);
+				comboBoxVender.addItem(cod);
+			}
+			stmt2.close();
+			rs2.close();
+			con.close();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			System.err.println(e.getMessage());
+			System.out.println("---------------------------------------------");
+			e.getErrorCode();
+			System.out.println("---------------------------------------------");
 			e.printStackTrace();
 		}
 
+		JButton btnMostrar = new JButton("Mostrar");
+		JButton btnAnyadir = new JButton("Añadir");
+		JButton btnActualizarPrecio = new JButton("Actualizar precio");
+		JButton btnActualizarStock = new JButton("Actualizar stock");
+		JButton btnVender = new JButton("Vender");
+
+		btnMostrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				try {
+
+					con = ConnectionSingleton.getConnection();
+
+					Statement stmt = con.createStatement();
+					ResultSet rs = stmt.executeQuery("SELECT * FROM productos");
+					model.setRowCount(0);
+					while (rs.next()) {
+						Object[] row = new Object[4];
+						row[0] = rs.getInt("codigo");
+						row[1] = rs.getString("nombre");
+						row[2] = rs.getDouble("precio");
+						row[3] = rs.getInt("unidades");
+						model.addRow(row);
+					}
+					stmt.close();
+					rs.close();
+
+					table.setModel(model);
+
+					comboBoxActualizar.removeAllItems();
+					comboBoxBorrar.removeAllItems();
+					comboBoxStock.removeAllItems();
+					comboBoxVender.removeAllItems();
+
+					Statement stmt2 = con.createStatement();
+					ResultSet rs2 = stmt2.executeQuery("SELECT codigo FROM productos");
+
+					while (rs2.next()) {
+						int cod = rs2.getInt("codigo");
+						comboBoxActualizar.addItem(cod);
+						comboBoxBorrar.addItem(cod);
+						comboBoxStock.addItem(cod);
+						comboBoxVender.addItem(cod);
+					}
+					stmt2.close();
+					rs2.close();
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+
+			}
+		});
+		btnMostrar.setBounds(189, 39, 117, 25);
+		frmTienda.getContentPane().add(btnMostrar);
+
+		btnAnyadir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				try {
+					con = ConnectionSingleton.getConnection();
+
+					int cod = Integer.valueOf(txtCodigo.getText());
+					String nombre = txtNombre.getText();
+					double precio = Double.valueOf(txtPrecio.getText());
+					;
+					int unidades = Integer.valueOf(txtUnidades.getText());
+
+					PreparedStatement ps = con.prepareStatement("INSERT INTO productos values (?, ?, ?, ?)");
+					ps.setInt(1, cod);
+					ps.setString(2, nombre);
+					ps.setDouble(3, precio);
+					ps.setInt(4, unidades);
+
+					int rowsInserted = ps.executeUpdate();
+					ps.close();
+					con.close();
+					btnMostrar.doClick();
+				} catch (SQLException e) {
+					System.err.println(e.getMessage());
+					System.out.println("---------------------------------------------");
+					e.getErrorCode();
+					System.out.println("---------------------------------------------");
+					e.printStackTrace();
+				}
+
+			}
+		});
+		btnAnyadir.setBounds(175, 324, 117, 25);
+		frmTienda.getContentPane().add(btnAnyadir);
+
+		btnEliminar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					con = ConnectionSingleton.getConnection();
+
+					int cod = Integer.valueOf(comboBoxBorrar.getSelectedItem().toString());
+
+					PreparedStatement dele_pstmt = con.prepareStatement("DELETE FROM productos WHERE codigo = ?");
+					dele_pstmt.setInt(1, cod);
+					int rowsDeleted = dele_pstmt.executeUpdate();
+
+					dele_pstmt.close();
+					con.close();
+					btnMostrar.doClick();
+				} catch (SQLException e) {
+					System.err.println(e.getMessage());
+					System.out.println("---------------------------------------------");
+					e.getErrorCode();
+					System.out.println("---------------------------------------------");
+					e.printStackTrace();
+				}
+
+			}
+		});
+		btnEliminar.setBounds(217, 388, 117, 25);
+		frmTienda.getContentPane().add(btnEliminar);
+
+		btnActualizarPrecio.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				try {
+					con = ConnectionSingleton.getConnection();
+					double precio = Double.valueOf(txtNuevoprecio.getText());
+					int cod = Integer.valueOf(comboBoxActualizar.getSelectedItem().toString());
+
+					PreparedStatement update_pstmt = con
+							.prepareStatement("UPDATE productos SET precio = ? WHERE codigo = ?");
+
+					update_pstmt.setDouble(1, precio);
+					update_pstmt.setInt(2, cod);
+
+					int rowsDeleted = update_pstmt.executeUpdate();
+
+					update_pstmt.close();
+					con.close();
+					btnMostrar.doClick();
+
+				} catch (SQLException e) {
+					System.err.println(e.getMessage());
+					System.out.println("---------------------------------------------");
+					e.getErrorCode();
+					System.out.println("---------------------------------------------");
+					e.printStackTrace();
+				}
+
+			}
+		});
+		btnActualizarPrecio.setBounds(690, 98, 164, 25);
+		frmTienda.getContentPane().add(btnActualizarPrecio);
+
+		btnActualizarStock.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				try {
+					con = ConnectionSingleton.getConnection();
+					int stock = Integer.valueOf(txtStock.getText());
+					int cod = Integer.valueOf(comboBoxActualizar.getSelectedItem().toString());
+					int ud = 0;
+					PreparedStatement ps = con.prepareStatement("SELECT unidades FROM productos WHERE codigo = ?");
+					ps.setInt(1, cod);
+					ResultSet rs = ps.executeQuery();
+
+					while (rs.next()) {
+						ud = rs.getInt("unidades");
+					}
+
+					stock += ud;
+
+					PreparedStatement update_pstmt = con
+							.prepareStatement("UPDATE productos SET unidades = ? WHERE codigo = ?");
+
+					update_pstmt.setDouble(1, stock);
+					update_pstmt.setInt(2, cod);
+
+					int rowsDeleted = update_pstmt.executeUpdate();
+
+					update_pstmt.close();
+					con.close();
+					btnMostrar.doClick();
+				} catch (SQLException e) {
+					System.err.println(e.getMessage());
+					System.out.println("---------------------------------------------");
+					e.getErrorCode();
+					System.out.println("---------------------------------------------");
+					e.printStackTrace();
+				}
+
+			}
+		});
+		btnActualizarStock.setBounds(686, 263, 168, 25);
+		frmTienda.getContentPane().add(btnActualizarStock);
+
+		btnVender.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				try {
+					con = ConnectionSingleton.getConnection();
+					int udVender = Integer.valueOf(textFieldUdVender.getText());
+					int cod = Integer.valueOf(comboBoxVender.getSelectedItem().toString());
+					int ud = 0;
+					int precio = 0;
+					PreparedStatement ps = con.prepareStatement("SELECT * FROM productos WHERE codigo = ?");
+					ps.setInt(1, cod);
+					ResultSet rs = ps.executeQuery();
+
+					while (rs.next()) {
+						ud = rs.getInt("unidades");
+						precio = rs.getInt("precio");
+					}
+
+					if (ud < udVender) {
+						throw new SQLException("Unidades insuficientes");
+					}
+
+					PreparedStatement update_pstmt = con
+							.prepareStatement("UPDATE productos SET unidades = ? WHERE codigo = ?");
+
+					ud -= udVender;
+					profit += udVender * precio;
+
+					update_pstmt.setDouble(1, ud);
+					update_pstmt.setInt(2, cod);
+
+					int rowsDeleted = update_pstmt.executeUpdate();
+					update_pstmt.close();
+					con.close();
+
+					labelGanancias.setText(String.valueOf(profit));
+					rs.close();
+					ps.close();
+					update_pstmt.close();
+					con.close();
+					JOptionPane.showMessageDialog(frmTienda, "Operacion realizada");
+
+					btnMostrar.doClick();
+				} catch (SQLException e) {
+					JOptionPane.showMessageDialog(frmTienda, "Operacion fallida "+e.getMessage(), null, JOptionPane.ERROR_MESSAGE);
+
+				}
+
+			}
+		});
+		btnVender.setBounds(733, 388, 108, 25);
+		frmTienda.getContentPane().add(btnVender);
 	}
 }
